1f024f5d3225a68e7992bdc80c7ea5280712f417
Kristina <chr1st1nko007@gmail.com>
2022-10-21 18:40:50 -0400

    add innerHTML

--- scripts.js
+++ scripts.js
@@ -15,9 +15,30 @@ const list = [
     },
 ];
 
+function render(){
+
+
 list.forEach(el => {
     const listItem = document.createElement('li');
     const listItemText = document.createTextNode(el.title);
     listItem.appendChild(listItemText);
     document.getElementById('list').appendChild(listItem);
 });
+
+}
+
+render();
+
+
+
+function addToList() {
+  const todoInputValue = document.getElementById('todoInput').value;
+
+  list.push({
+      id: Math.random(),
+      title: todoInputValue
+  })
+    console.log(list);
+document.getElementById('list').innerHTML = null;
+    render();
+}
